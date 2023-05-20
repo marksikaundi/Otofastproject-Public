ea57940f28074d7abb9479b9bd6628c70b4782c8
marksikaundi <emmanuelmark117@gmail.com>
2023-05-20 22:45:45 +0200

    configs

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,32 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Header from "@/components/Header";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+import Providers from "./Providers";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'OtofastProject',
-  description: 'Platform for digital contents',
-}
+  title: "OtofastProject",
+  description: "Platform for digital contents",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Providers>
+          {/* Header */}
+          <Header />
 
-      {children}
+          {/* NavBar */}
+          <NavBar />
+
+          {children}
+          <Footer />
+        </Providers>
       </body>
     </html>
-  )
+  );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import MenuItem from "./MenuItem";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import Link from "next/link";
+import DarkModeSwitch from "./DarkModeSwitch";
+
+export default function Header() {
+  return (
+    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
+      <div className="flex">
+        <MenuItem title="HOME" address="/" Icon={AiFillHome} />
+
+        <MenuItem title="FEATURES" address="/features" Icon={BsFillInfoCircleFill} />
+        <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
+      </div>
+
+      <div className="flex items-center space-x-5">
+        <DarkModeSwitch />
+        <Link href="/">
+          <h2 className="text-2xl">
+            <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
+              Otofast
+            </span>
+            <span className="text-xl hidden sm:inline"></span>
+          </h2>
+        </Link>
+      </div>
+    </div>
+  );
+}
